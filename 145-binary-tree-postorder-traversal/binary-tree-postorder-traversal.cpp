4871f6205aa45881fe455c6c5dba2ffb8110acb0
Deepu4402020 <161422752+Deepu4402020@users.noreply.github.com>
2025-01-17 14:48:24 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (11.78%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -6,19 +6,38 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 
 class Solution {
 public:
-    vector<int> num={};
     vector<int> postorderTraversal(TreeNode* root) {
-        if (root==nullptr)
-            return num;
-        postorderTraversal(root->left);
-        postorderTraversal(root->right);
-        num.push_back(root->val);
-        return num;
+        stack<TreeNode*> stk;
+        stack<TreeNode*> stk2;
+        TreeNode* current = root;
+        if (current)
+            stk.push(current);
+
+        while (!stk.empty()) {
+            current = stk.top();
+            stk.pop();
+            stk2.push(current);
+
+            if (current->left) {
+                stk.push(current->left);
+            }
+            if (current->right) {
+                stk.push(current->right);
+            }
+        }
+
+        vector<int> vec;
+        while (!stk2.empty()) {
+            vec.push_back(stk2.top()->val);
+            stk2.pop();
+        }
+        return vec;
     }
 };
