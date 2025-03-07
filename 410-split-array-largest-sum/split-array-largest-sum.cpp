11b691c728bc82c9756015712cc17fa50ef1baa8
Deepu4402020 <161422752+Deepu4402020@users.noreply.github.com>
2025-03-08 00:18:06 +0530

    Time: 3 ms (10.76%) | Memory: 10.4 MB (55.38%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,45 +1,45 @@
 class Solution {
-public:       
- int countsubarr(vector<int>& a,int maxsum,int n){
-        int count=1;
-        long long subarrsum=0;
-        for(int i=0;i<n;i++){
-            if(subarrsum+a[i]<=maxsum)
-            subarrsum+=a[i];
-            else{
-                subarrsum=a[i];
-                count++;
+public:
+
+    int ifPossible(vector<int>& nums, int mid)
+    {
+        int curr = 1;
+        int sum=0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(sum+nums[i]<=mid)
+            {
+                sum += nums[i];
+
+            }
+            else
+            {
+                curr++;
+                sum=nums[i];
             }
         }
-        return count;
+        return curr;
     }
-    int sumarr(vector<int>& a,int n){
-        int sum=0;
-        for(int i=0;i<n;i++)
-        sum+=a[i];
-        return sum;
-    }
-    int max_element(vector<int>& a, int n){
-        int maxi=a[0];
-        for(int i=1;i<n;i++){
-            maxi=max(maxi,a[i]);
-        }
-        return maxi;
-    }
-    int splitArray(vector<int>& a, int k) {
-        if(a.size()<k)
-        return -1;
-        int n=a.size();
-        int low=max_element(a,n);
-        int high=sumarr(a,n);
-        while(low<=high){
-            int mid=(low+high)/2;
-            int cnt=countsubarr(a,mid,n);
-            if(cnt<=k)
-            high=mid-1;
+
+    int splitArray(vector<int>& nums, int k) {
+        int s = *max_element(nums.begin(),nums.end());
+        int e = accumulate(nums.begin(),nums.end(),0);
+
+        while(s<=e)
+        {
+            int mid = s + (e-s) /2;
+
+            int ans = ifPossible(nums,mid);
+
+            if(ans<=k)
+            {
+                e=mid-1;
+            }
             else
-            low=mid+1;
+            {
+               s=mid+1;
+            }
         }
-        return low;
+        return s;
     }
 };
