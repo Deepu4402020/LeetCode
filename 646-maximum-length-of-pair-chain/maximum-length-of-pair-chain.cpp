e5572ae883289310450a56343748a8ab71956999
Deepu4402020 <161422752+Deepu4402020@users.noreply.github.com>
2025-09-22 18:23:15 +0530

    Time: 307 ms (8.60%) | Memory: 163.2 MB (15.10%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,25 +1,29 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
+
+//Rec+Memo
+    int solve(int i, vector<vector<int>>& pairs, vector<vector<int>>& memo, int prevSel_idx) {
+        if (i >= pairs.size()) return 0;
+
+        int prev = (prevSel_idx == -1 ? pairs.size() : prevSel_idx);
+
+        if (memo[i][prev] != -1) return memo[i][prev];
+
+        // Select
+        int take = (prevSel_idx == -1 || pairs[prevSel_idx][1] < pairs[i][0])
+                   ? 1 + solve(i+1, pairs, memo, i) : 0;
+
+        // Skip
+        int skip = solve(i+1, pairs, memo, prevSel_idx);
+
+        return memo[i][prev] = max(take, skip);
+    }
+
     int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(), pairs.end());  
         
-        sort(pairs.begin(), pairs.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[1] < b[1];
-        });
-
-        int count = 0;
-        int curr_end = INT_MIN;
-
-        for (auto &p : pairs) {
-            if (p[0] > curr_end) {
-                count++;
-                curr_end = p[1];
-            }
-        }
-
-        return count;
+        int n = pairs.size();
+        vector<vector<int>> memo(n, vector<int>(n+1, -1));
+        return solve(0, pairs, memo, -1);
     }
 };
