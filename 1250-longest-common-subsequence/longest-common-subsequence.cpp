c69c2e96fb00aa3527a2b7b085add16022381826
Deepu4402020 <161422752+Deepu4402020@users.noreply.github.com>
2025-10-08 17:41:19 +0530

    Time: 20 ms (84.14%) | Memory: 12.7 MB (88.12%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int memo[1001][1001];
+    int m, n;
 
-    int solve(string &s, string &t, int si, int ti) {
-        if (si >= s.size() || ti >= t.size()) {
-            return 0;
+    int solve(string& s, string& t) {
+        // Initialize first row and column to 0
+        for (int i = 0; i <= m; i++) memo[i][0] = 0;
+        for (int j = 0; j <= n; j++) memo[0][j] = 0;
+
+       // i ,j are length so index would be i-1,j-1
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    memo[i][j] = 1 + memo[i - 1][j - 1]; // take match
+                } else {
+                    memo[i][j] = max(memo[i - 1][j], memo[i][j - 1]); // skip
+                }
+            }
         }
 
-        if (memo[si][ti] != -1) {
-            return memo[si][ti];
-        }
-
-        // can take
-        if (s[si] == t[ti]) {
-            return memo[si][ti] = 1 + solve(s, t, si + 1, ti + 1);
-        }
-
-        // skip 1st char
-        int x = solve(s, t, si + 1, ti);
-        int y = solve(s, t, si, ti + 1);
-
-        return memo[si][ti] = max(x, y);
+        return memo[m][n]; 
     }
 
     int longestCommonSubsequence(string s, string t) {
-        memset(memo, -1, sizeof(memo));  
-        return solve(s, t, 0, 0);
+        m = s.size();
+        n = t.size();
+        return solve(s, t);
     }
 };
