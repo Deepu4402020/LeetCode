7563f7dcbb653e0dadd389bcfe3c8030621de884
Deepesh <161422752+deepu4402020@users.noreply.github.com>
2025-12-22 13:31:41 +0530

    Time: 39 ms (42.64%) | Memory: 42.8 MB (17.94%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -18,9 +18,29 @@ public:
         return dp[i][idx]=ans;
     }
 
-    int findTargetSumWays(vector<int>& nums, int target) {
+    int findTargetSumWays(vector<int>& nums, int t) {
         n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(2001,-1));
-        return solve(nums, target, 0, 0,dp);
+        vector<vector<int>>dp(n+1,vector<int>(2001,0));
+         
+         //BASE CASE
+          dp[n][t+1000]=1;
+
+          //Recur simulaiton
+        for (int i = n - 1; i >= 0; i--) {
+            for (int cur_sum = -1000; cur_sum <= 1000; cur_sum++) {
+                int idx = cur_sum + 1000;
+
+                int plus = cur_sum + nums[i];
+                int minus = cur_sum - nums[i];
+
+                if (plus >= -1000 && plus <= 1000)
+                    dp[i][idx] += dp[i + 1][plus + 1000];
+
+                if (minus >= -1000 && minus <= 1000)
+                    dp[i][idx] += dp[i + 1][minus + 1000];
+            }
+        }
+        return dp[0][1000];
+
     }
 };
